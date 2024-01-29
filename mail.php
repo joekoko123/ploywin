481690698cc97392db34047c66ef1e0ba770be93
unknown <maroun233243@gmail.com>
2024-01-29 15:19:53 +0200

    changed index video

--- mail.php
+++ mail.php
@@ -34,7 +34,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Send email and check for success
     if ($mail->send()) {
         // Redirect to the referring page upon success
-        // header("Location: {$_SERVER['HTTP_REFERER']}?email=sent?status=success");
+        header("Location: {$_SERVER['HTTP_REFERER']}?email=sent?status=success");
         exit;
     } else {
         // Redirect back to the index page upon failure
